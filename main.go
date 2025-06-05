d81e6f08222416a8b54cdf2e3a8ed08c2a8a940b
MCW <759416866@qq.com>
2025-06-05 13:31:34 +0800

    变更

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
-import (
-	"log"
-	"os/exec"
-)
-
 func main() {
-	e := exec.Command("ls", "-l")
-	b, err1 := e.Output()
-	if err1 != nil {
-		log.Println(err1)
+	numTilings(10)
+
+}
+
+func numTilings(n int) int {
+	var mod int = 1e9 + 7
+	dp := [3]int{1, 0, 0}
+	for i := 1; i <= n; i++ {
+		temp := [3]int{0, 0, 0}
+		temp[0] = (dp[0] + dp[1]*2 + dp[2]) % mod
+		temp[1] = (dp[1] + dp[2]) % mod
+		temp[2] = dp[0]
+		dp = temp
 	}
-	log.Println(string(b))
+	return dp[0]
+}
+
+type H struct {
+	nums []int
+}
+
+func (h *H) Len() int {
+	return len(h.nums)
+}
+
+func (h *H) Less(i, j int) bool {
+	return h.nums[i] < h.nums[j]
+}
+
+func (h *H) Swap(i, j int) {
+	h.nums[i], h.nums[j] = h.nums[j], h.nums[i]
+}
+
+func (h *H) Push(_ any) {}
+
+func (h *H) Pop() (_ any) {
+	//x := h.nums[len(h.nums)-1]
+	h.nums = h.nums[:len(h.nums)-1]
+	return
 }
